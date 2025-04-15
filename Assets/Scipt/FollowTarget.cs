1bbc849b9417d3151e4ede2cdd4f105ca2365bac
Nathan.Kelly <spinyattack17@gmail.com>
2025-04-15 17:48:24 -0400

    camera sript

--- Assets/Scipt/FollowTarget.cs
+++ Assets/Scipt/FollowTarget.cs
@@ -7,18 +7,17 @@ public class FollowTarget : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform playerTransform;
-    private Vector3 offset;
-    private Quaternion rotationOffset;
+    
+    
 
     private void Start()
     {
-        offset = transform.position - playerTransform.position;
         //rotationOffset = Quaternion.Inverse(playerTransform.rotation) * transform.rotation;
     }
 
     private void Update()
     {
-        transform.position = playerTransform.position + offset;
+        transform.position = playerTransform.position;
         //transform.rotation = playerTransform.rotation * rotationOffset;
     }
 }
