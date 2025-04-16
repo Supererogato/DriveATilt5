de272cd586d5bd441d661982e4298917f87eb55f
Supererogato <terrariaprogamer2.0@gmail.com>
2025-04-15 20:26:16 -0400

    Scene changes, prefab changes and minor script changes

--- Assets/Scipt/FollowTarget.cs
+++ Assets/Scipt/FollowTarget.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
 
 public class FollowTarget : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform playerTransform;
+    // public float transformOffsetX;
+    // public float transformOffsetY;
+    // public float transformOffsetZ;
 
     private void Start()
     {
-        
+        //var Tilt5Offset = new Vector3(transformOffsetX, transformOffsetY, transformOffsetZ);
     }
 
     private void Update()
